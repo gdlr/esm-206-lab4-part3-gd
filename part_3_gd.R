88b58e9933e7d4b2e292e7f71e7c5e1b9b6815d0
gdlr <gdelarosa010@gmail.com>
2019-10-21 13:56:33 -0700

    Made graph

--- part_3_gd.R
+++ part_3_gd.R
@@ -16,3 +16,15 @@ db <- read_csv(here::here("data", "disease_burden.csv"))
 clean_db <- db %>% 
   clean_names() %>% 
   rename(deaths_per_100k = death_rate_per_100_000)
+
+# Create a subset, then graph
+
+db_sub <- clean_db %>%
+  filter(country_name %in% c("Afghanistan", "United States", "Japan", "Somalia")) %>%
+  filter(age_group == "0-6 days", sex == "Both")
+
+# Create basic linegraph
+
+ggplot(data = db_sub, aes(x = year, y = deaths_per_100k)) +
+  geom_line(aes(color = country_name))
+
